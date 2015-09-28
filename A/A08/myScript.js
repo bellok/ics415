59da82c0e148eea9bc4ca40a76abc71453e169c6
bellok <kbello_19@yahoo.com>
2015-09-27 20:48:14 -1000

    Minor corrections for A08

--- A/A08/myScript.js
+++ A/A08/myScript.js
@@ -1,6 +1,8 @@
 /**
  * Created by Kaiewa on 9/27/2015.
  */
+
+// Assistance received from Aljon Preza and Spencer Allen
 $(document).ready(function() {
     $(".panel-body").hide();
     $(".up").hide();
@@ -10,6 +12,13 @@ $(document).ready(function() {
     });
 });
 
+// Solution derived from
+// http://stackoverflow.com/questions/17983348/how-to-get-div-content-from-another-page-using-ajax
+/**
+ * NumLinks receives the data from the urlName provided upon
+ * @returns {boolean}
+ * @constructor
+ */
 function Numlinks() {
     event.preventDefault();
     var urlName = document.getElementById("urlName").value;
